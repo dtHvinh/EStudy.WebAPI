a445639ad89b51f676230e96618a6e49c48bd9c2
dtHvinh <dangthehienvinh662@gmail.com>
2025-06-01 13:59:49 +0700

    Add update blog endpoint and related DTOs
    Introduces a new endpoint for updating blog posts using the FastEndpoints library and Entity Framework Core. The endpoint validates the existence of the blog and user permissions before updating the blog with data from the new UpdateBlogRequest DTO.
    
    Also adds a Mapper class to handle property mapping from UpdateBlogRequest to Blog, ensuring the blog's Id remains unchanged during updates.

---
+++ WebAPI/Endpoints/BlogEndpoints/UpdateBlog/Endpoint.cs
@@ -0,0 +1,46 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Utilities.Extensions;
+
+namespace WebAPI.Endpoints.BlogEndpoints.UpdateBlog;
+
+public class Endpoint(ApplicationDbContext context) : Endpoint<UpdateBlogRequest>
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public override void Configure()
+    {
+        Put("{id}");
+        Group<BlogGroup>();
+    }
+
+    public override async Task HandleAsync(UpdateBlogRequest req, CancellationToken ct)
+    {
+        var blog = await _context.Blogs
+            .Include(e => e.Author)
+            .FirstOrDefaultAsync(e => e.Id == req.Id, cancellationToken: ct);
+
+        if (blog is null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        if (blog.AuthorId != int.Parse(this.RetrieveUserId()))
+            await SendForbiddenAsync(ct);
+
+        blog.UpdateFrom(req);
+
+        _context.Blogs.Update(blog);
+
+        if (await _context.SaveChangesAsync(ct) == 1)
+        {
+            await SendNoContentAsync(ct);
+        }
+        else
+        {
+            ThrowError("Failed to update blog");
+        }
+    }
+}
---
+++ WebAPI/Endpoints/BlogEndpoints/UpdateBlog/Mapper.cs
@@ -0,0 +1,11 @@
+﻿using Riok.Mapperly.Abstractions;
+using WebAPI.Models;
+
+namespace WebAPI.Endpoints.BlogEndpoints.UpdateBlog;
+
+[Mapper(AllowNullPropertyAssignment = false)]
+public static partial class Mapper
+{
+    [MapperIgnoreSource(nameof(Blog.Id))]
+    public static partial void UpdateFrom([MappingTarget] this Blog target, UpdateBlogRequest newData);
+}
