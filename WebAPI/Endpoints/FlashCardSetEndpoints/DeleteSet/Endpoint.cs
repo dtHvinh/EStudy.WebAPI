7c1f61a44083ac97caee8a41b66d764dd77cab6b
dtHvinh <dangthehienvinh662@gmail.com>
2025-06-01 18:28:46 +0700

    Add delete endpoint for flash card sets
    Introduces a new endpoint in `Endpoint.cs` for deleting flash card sets. Implements the `Configure` and `HandleAsync` methods to handle deletion logic, including checks for existence and ownership. Adds `DeleteSetRequest` class in `Models.cs` to encapsulate the request data.

---
+++ WebAPI/Endpoints/FlashCardSetEndpoints/DeleteSet/Endpoint.cs
@@ -0,0 +1,40 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Utilities.Extensions;
+
+namespace WebAPI.Endpoints.FlashCardSetEndpoints.DeleteSet;
+
+public class Endpoint(ApplicationDbContext context) : Endpoint<DeleteSetRequest>
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public override void Configure()
+    {
+        Delete("{id}");
+        Group<FlashCardSetGroup>();
+    }
+
+    public override async Task HandleAsync(DeleteSetRequest req, CancellationToken ct)
+    {
+        var set = await _context.FlashCardSets.Include(e => e.Author).FirstOrDefaultAsync(e => e.Id == req.Id, ct);
+
+        if (set is null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        if (!set.IsBelongTo(this.RetrieveUserId()))
+        {
+            await SendForbiddenAsync(ct);
+        }
+
+        _context.FlashCardSets.Remove(set);
+
+        if (await _context.SaveChangesAsync(ct) == 1)
+            await SendNoContentAsync(ct);
+
+        ThrowError("Unable to delete set");
+    }
+}
---
+++ WebAPI/Endpoints/FlashCardSetEndpoints/DeleteSet/Models.cs
@@ -0,0 +1,6 @@
+﻿namespace WebAPI.Endpoints.FlashCardSetEndpoints.DeleteSet;
+
+public sealed class DeleteSetRequest
+{
+    public int Id { get; set; }
+}
