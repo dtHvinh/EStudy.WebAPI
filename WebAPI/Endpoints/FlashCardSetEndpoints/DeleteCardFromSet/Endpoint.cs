944fed0ed3cb82894da62b5dad4a23abf9618698
dtHvinh <dangthehienvinh662@gmail.com>
2025-06-03 10:43:19 +0700

    Add endpoint to delete card from flashcard set
    Introduces a new endpoint for deleting a card from a flashcard set in `Endpoint.cs`. Implements the `Configure` and `HandleAsync` methods to check for the existence of the set and card, remove the card if found, and handle responses. Also adds a new request model `DeleteCardFromSetRequest` in `Models.cs` with properties for `CardId` and `SetId`.

---
+++ WebAPI/Endpoints/FlashCardSetEndpoints/DeleteCardFromSet/Endpoint.cs
@@ -0,0 +1,45 @@
+﻿using FastEndpoints;
+using WebAPI.Data;
+
+namespace WebAPI.Endpoints.FlashCardSetEndpoints.DeleteCardFromSet;
+
+public class Endpoint(ApplicationDbContext context) : Endpoint<DeleteCardFromSetRequest>
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public override void Configure()
+    {
+        Delete("/{setId}/remove/{cardId}");
+        Group<FlashCardSetGroup>();
+    }
+
+    public override async Task HandleAsync(DeleteCardFromSetRequest req, CancellationToken ct)
+    {
+        var set = await _context.FlashCardSets.FindAsync([req.SetId], ct);
+
+        if (set is null)
+        {
+            ThrowError("Set not found");
+            return;
+        }
+
+        var card = await _context.FlashCards.FindAsync([req.CardId], ct);
+
+        if (card is null)
+        {
+            ThrowError("Card not found");
+            return;
+        }
+
+        set.FlashCards.Remove(card);
+
+        if (await _context.SaveChangesAsync(ct) == 1)
+        {
+            await SendNoContentAsync(ct);
+        }
+        else
+        {
+            ThrowError("Failed to remove card from set");
+        }
+    }
+}
---
+++ WebAPI/Endpoints/FlashCardSetEndpoints/DeleteCardFromSet/Models.cs
@@ -0,0 +1,7 @@
+﻿namespace WebAPI.Endpoints.FlashCardSetEndpoints.DeleteCardFromSet;
+
+public sealed class DeleteCardFromSetRequest
+{
+    public int CardId { get; set; }
+    public int SetId { get; set; }
+}
